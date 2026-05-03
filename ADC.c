0e27b2ee49afb93a45ee836254a54db2985a2dfb
Vanegas <allison.vanegas.607@my.csun.edu>
2026-05-02 21:17:12 -0700

    update

--- ADC.c
+++ ADC.c
@@ -5,6 +5,7 @@ void ADC_Init(void)
     // Clocks
     SYSCTL->RCGCADC |= 0x01;  // Enable clock to ADC Module 0
     SYSCTL->RCGCGPIO |= 0x10; // Enable clock to Port E
+	  
     
     // Dumb delay for clock stabilization
     for(volatile int i = 0; i < 1000; i++); 
@@ -14,30 +15,49 @@ void ADC_Init(void)
     GPIOE->AFSEL |= 0x06;     // Enable alternate function on PE1
     GPIOE->DEN &= ~0x06;      // Disable digital functionality on PE1
     GPIOE->AMSEL |= 0x06;     // Enable analog on PE1
+	
+	  // Pins (Configuring PE3 and PE4)
+    GPIOE->DIR &= ~0x18; // Set PE3 and PE4 as inputs (Bit 3 & 4 = 0x18)
+    GPIOE->AFSEL |= 0x18; // Enable alternate function on PE3 and PE4
+    GPIOE->DEN &= ~0x18;  // Disable digital functionality on PE3 and PE4
+    GPIOE->AMSEL |= 0x18; // Enable analog on PE3 and PE4
+	
+	  // Configure PE5 and PE0
+    GPIOE->DIR &= ~0x21; // Set PE5 as input (Bit 5 = 0x20)
+    GPIOE->AFSEL |= 0x21; // Enable alternate function on PE5
+    GPIOE->DEN &= ~0x21;  // Disable digital functionality on PE5
+    GPIOE->AMSEL |= 0x21; // Enable analog on PE5
+	   
+	  // Parameters
+     ADC0->ACTSS &= ~0x01;        // Disable Sample Sequencer 0 (SS0) before setup
+     ADC0->EMUX &= ~0x000F;       // Select software trigger for SS0 
+
+     // Configure the multiplexer to route 4 pins to SS0
+     ADC0->SSMUX0 &= ~0xFFFFFFFF; // Clear all slots
+     ADC0->SSMUX0 |= 0x00389021;  // Route PE2, PE1, PE3, PE4, PE5, PE0 (AIN1, AIN2, AIN0, AIN9, AIN8, AIN3)
+
+     // Configure the control bits for the 6th sample in SS0
+     ADC0->SSCTL0 = 0x00600000;   // Set IE3 and END3 (Stop after 6th sample)
+
+     // Enable
+     ADC0->ACTSS |= 0x01;         // Turn Sample Sequencer 0 (SS0) back on
     
-    // Parameters
-    ADC0->ACTSS &= ~0x01;     // Disable Sample Sequencer 3 (SS3) before setup
-    ADC0->EMUX &= ~0x000F;   // Select software trigger for SS3 
     
-    // Configure the multiplexer to route AIN2 (PE1) to SS3
-    ADC0->SSMUX0 &= ~0xFFFFFFFF;
-		ADC0->SSMUX0 |= 0x00000021; // Clear the 1st sample slot 
-   
-    
-    // Configure the control bits for the 1st (and only) sample in SS3
-     ADC0->SSCTL0 = 0x00000060;  
-    
-    // Enable
-		ADC0->ACTSS |= 0x01;// Turn Sample Sequencer 3 (SS3) back on
 }
 
 void ADC_Sample(uint32_t buffer[])
 {
-    ADC0->PSSI = 0x01;            // trigger SS0
-    while((ADC0->RIS & 0x01) == 0){}
+    ADC0->PSSI = 0x01;            // Trigger SS0
+    while((ADC0->RIS & 0x01) == 0){} // Wait for the flag
 
-    buffer[0] = ADC0->SSFIFO0 & 0xFFF;   // first sample PE2
-    buffer[1] = ADC0->SSFIFO0 & 0xFFF;   // second sample PE1
+    // Must be read in the order defined by SSMUX0 (1, 2, 0, 9)
+    buffer[0] = ADC0->SSFIFO0 & 0xFFF;   // 1st sample: AIN1 (PE2)
+    buffer[1] = ADC0->SSFIFO0 & 0xFFF;   // 2nd sample: AIN2 (PE1)
+    buffer[2] = ADC0->SSFIFO0 & 0xFFF;   // 3rd sample: AIN0 (PE3)
+    buffer[3] = ADC0->SSFIFO0 & 0xFFF;   // 4th sample: AIN9 (PE4)
+		buffer[4] = ADC0->SSFIFO0 & 0xFFF;   // 5th sample: AIN8 (PE5)
+		buffer[5] = ADC0->SSFIFO0 & 0xFFF;   // 6th sample: AIN3 (PE0)
+			
 
-    ADC0->ISC = 0x01;             // clear SS0 flag
+    ADC0->ISC = 0x01;             // Clear SS0 flag
 }
--- main.c
+++ main.c
@@ -24,8 +24,16 @@ int main(void){
 	//uint32_t adc_result;
 	uint32_t high_ticks0;
 	uint32_t high_ticks1;
+	uint32_t high_ticks2;
+	uint32_t high_ticks3;
+	uint32_t high_ticks4;
+	uint32_t high_ticks5;
 	uint32_t new_compare0;
 	uint32_t new_compare1;
+	uint32_t new_compare2;
+	uint32_t new_compare3;
+	uint32_t new_compare4;
+	uint32_t new_compare5;
 	
 	// 2. Main Loop Sweep the Servo
 	while(1)
@@ -34,15 +42,31 @@ int main(void){
     
     high_ticks0 = 125 + ((adc_buffer[0] * 500) / 4095);
     high_ticks1 = 125 + ((adc_buffer[1] * 500) / 4095);
+		high_ticks2 = 125 + ((adc_buffer[2] * 500) / 4095);
+		high_ticks3 = 125 + ((adc_buffer[3] * 500) / 4095);
+		high_ticks4 = 125 + ((adc_buffer[4] * 500) / 4095);
+		high_ticks5 = 125 + ((adc_buffer[5] * 500) / 4095);
 
     // 6. Calculate CMPA (Since the timer counts DOWN)
     new_compare0 = 5000 - high_ticks0;
 		new_compare1 = 5000 - high_ticks1;
+		new_compare2 = 5000 - high_ticks2;
+		new_compare3 = 5000 - high_ticks3;
+		new_compare4 = 5000 - high_ticks4;
+		new_compare5 = 5000 - high_ticks5;
     
     // 7. Update the PWM hardware to move the servo!
-    PWM1->_3_CMPB = new_compare0;//PE2 with PF3
+    PWM0->_1_CMPA = new_compare0;//PE2 with PB4
 		
     PWM0->_1_CMPB = new_compare1;//PE1 with PB5
+		
+		PWM0->_0_CMPA = new_compare2;//PE3 with PB6 something wrong..
+		
+		PWM1->_3_CMPB = new_compare3;//PE4 with PF3
+		
+		PWM1->_2_CMPA = new_compare4;//PE5 with PF0
+		
+		PWM1->_2_CMPB = new_compare5;//PE0 with PF1
     //delay
 		for(volatile int i = 0; i < 40000; i++);
 	  //SysTick_Delay1ms(20);
